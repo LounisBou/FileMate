0ffe5ac8bce8bab85722ba58ea0757c12f920885
LounisBou <lounis.bou@gmail.com>
2025-11-02 10:40:13 +0100

    Update setup, use _about.py

---
+++ filemate/_about.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" Module about information. """
+
+__package_name__ = "filemate"
+__version__ = "0.0.1"
+__author__ = "LounisBou"
+__email__ = "lounis.bou@gmail.com"
+__description__ = "A NAS FileSystem management tool."
+__license__ = "Apache-2.0"
+__url__ = "https://github.com/LounisBou/filemate"
